b74491fc75ecf66b206affe8743d867d14c515fe
Andrey Mendonça <andrey.mmendonca@gmail.com>
2023-12-08 15:58:32 -0300

    Conteudos de class

---
+++ 05 - ClassesComplementos/05 - ClassesComplementos/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _05___ClassesComplementos {
+    class Produto {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+        public void AdicionarEstoque(int quantidade) {
+            this.Quantidade += quantidade;
+        }
+        public void RemoverEstoque(int quantidade) {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString() {
+            return Nome
+                + " $ " + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + " Quantidade: " + Quantidade
+                + " Valor total em estoque: " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ 05 - ClassesComplementos/05 - ClassesComplementos/Program.cs
@@ -0,0 +1,22 @@
+﻿using _05___ClassesComplementos;
+using System;
+using System.Globalization;
+
+namespace Classe {
+    class Program {
+        static void Main(string[] args) {
+
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade de estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ddos do produto: " + p);
+        }
+    }
+}
