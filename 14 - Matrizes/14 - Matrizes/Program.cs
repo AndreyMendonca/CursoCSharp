2f8c6e89b63dee7697b740737e6d01cd5e395c84
Andrey Mendonça <andrey.mmendonca@gmail.com>
2023-12-11 22:36:09 -0300

    Matrizes

---
+++ 14 - Matrizes/14 - Matrizes/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Matrizes {
+    class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n, n];
+
+            for (int i = 0; i < n; i++) {
+                string[] vectot = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++) {
+                    matriz[i, j] = int.Parse(vectot[j]);
+                }
+            }
+
+            Console.WriteLine("Diagonal principal: ");
+            for (int i = 0; i < n; i++) {
+                Console.Write(matriz[i, i] + " ");
+            }
+
+            Console.WriteLine();
+            int contagem = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (matriz[i, j] < 0) {
+                        contagem++;
+                    }
+                }
+            }
+            Console.WriteLine("Número negativos: " + contagem);
+        }
+    }
+}
+
