97cf7e86b1d9ef22c31b2b220107ab3ef842e6de
Andrey Mendonça <andrey.mmendonca@gmail.com>
2023-12-26 22:51:12 -0300

    Herança

---
+++ 23 - Heranca/Heranca/Program.cs
@@ -0,0 +1,21 @@
+﻿using Heranca.Entities;
+
+namespace Heranca {
+    class Program {
+        static void Main(string[] args) {
+            Account acc1 = new(1, "Joao", 500.00);
+            BusinessAccount acc2 = new BusinessAccount(2, "Maria", 0.0, 100);
+            SavingsAccount acc3 = new SavingsAccount(3, "Carlos", 500.00, 1);
+
+            // Toda classs filha pode ser uma classe mae
+
+            acc1.Withdraw(100);
+            acc3.Withdraw(100);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+            Console.WriteLine(acc3.Balance);    
+
+        }
+    }
+}
