acac33cdcf030c0d64a6a70943d33832fb4207f8
Andrey Mendonça <andrey.mmendonca@gmail.com>
2023-12-27 23:09:55 -0300

    Exercicios

---
+++ 22- ExercicioFixacao/ExercicioFixacao/Entities/Order.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixacao.Entities {
+    class Order {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client client { get; set; }
+
+        public List<OrderItem> items { get; set; } =  new List<OrderItem>();
+
+        public Order() { }
+        public Order(DateTime moment, OrderStatus status, Client clinet) {
+            this.Moment = moment;
+            this.Status = status;
+            this.client = clinet;
+        }
+        public void AddItem(OrderItem item) {
+            items.Add(item);
+        }
+        public void RemoveItem(OrderItem item) {
+            items.Remove(item);
+        }
+        public double Total() {
+            double total = 0.0;
+            foreach (OrderItem i in items) {
+                total += i.SubTotal();
+            }
+            return total;
+        }
+
+        public override string ToString() {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Order moment: ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy hh:mm:ss"));
+            sb.Append("Order status: ");
+            sb.AppendLine("Status " + Status);
+            sb.AppendLine("Client " + client);
+            sb.AppendLine("Order Items: ");
+            foreach (OrderItem i in items) {
+                sb.Append(i);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ 22- ExercicioFixacao/ExercicioFixacao/Program.cs
@@ -0,0 +1,47 @@
+﻿using ExercicioFixacao.Entities;
+using System.Globalization;
+
+namespace ExercicioFixacao {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter cliente data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthday = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(name, email, birthday);
+
+            Console.WriteLine("Enter order data:");
+
+            Order order = new Order(DateTime.Now, OrderStatus.PROCESSING, client);
+
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine($"Enter #{i} item data");
+                Console.Write("Product name: ");
+                string product = Console.ReadLine();
+                Console.Write("Product prime: ");
+                double price = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                OrderItem oi = new OrderItem(quantity, price, new Product(name, price));
+
+                order.AddItem(oi);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+
+
+        }
+    }
+}
