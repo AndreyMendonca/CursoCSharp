fd7f2c4ed1b8123aef82c4b8eaeca22425da3417
Andrey Mendonça <andrey.mmendonca@gmail.com>
2024-01-01 22:46:08 -0300

    Tratamento de Excecoes

---
+++ 27 - TratamentoDeExcecao/TratamentoDeExcecao/Entities/Reservation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TratamentoDeExcecao.Entities.Exceptions;
+
+namespace TratamentoDeExcecao.Entities {
+    class Reservation {
+        public int RoomNumber {  get; set; }
+        public DateTime Checkin {  get; set; }
+        public DateTime Checkout {  get; set; }
+
+        public Reservation() { }
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout) {
+
+            if(checkin >= checkout) {
+                throw new DomainException("Check-out date must be after check-in date!");
+            }
+            
+            RoomNumber = roomNumber;
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public int Duration() {
+            TimeSpan duration = Checkout.Subtract(Checkin);
+            return (int)duration.TotalDays;
+        }
+
+        public void updateDates(DateTime checkin, DateTime checkout) {
+            DateTime now = DateTime.Now;
+            if(checkin < now || checkout < now) {
+                throw new DomainException("Reservation dates for update must be future dates");
+            }
+            if (checkin >= checkout) {
+                throw new DomainException("Check-out date must be after check-in date!");
+            }
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+        public override string ToString() {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + Checkin.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + Checkout.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+    }
+}
---
+++ 27 - TratamentoDeExcecao/TratamentoDeExcecao/Program.cs
@@ -0,0 +1,25 @@
+﻿using TratamentoDeExcecao.Entities;
+using TratamentoDeExcecao.Entities.Exceptions;
+
+namespace tratamento {
+    class Program {
+        static void Main(string[] strings) {
+            
+            try {
+                Console.Write("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy):");
+                DateTime checkout = DateTime.Parse(Console.ReadLine());
+                Reservation reservation = new Reservation(number, checkin, checkout);
+
+                Console.WriteLine(reservation);
+            }catch(DomainException e) {
+                Console.WriteLine(e.Message);
+            }
+            
+
+        }
+    }
+}
