f44f95312978efb47919025e429998505c954d64
Andrey Mendonça <andrey.mmendonca@gmail.com>
2024-01-11 22:41:10 -0300

    Generics

---
+++ 31 - GenericsSetDictionary/01_Generics/PrintService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Generics {
+    internal class PrintService<T> {
+        private T[] _value = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T value) {
+            if (_count++ == 10) {
+                throw new Exception("Erro!");
+            }
+            _value[_count] = value;
+            _count++;
+        }
+        public T First() {
+            if(_count == 0) {
+                throw new Exception("Vazio!");
+            }
+            return _value[0];
+        }
+        public void Print() {
+            if (_count == 0) {
+                throw new Exception("Vazio!");
+            }
+            for(int i = 0; i < _count- 1; i++) {
+                Console.Write(_value[i] + ", ");
+            }
+            if (_count > 0) {
+                Console.Write(_value[_count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ 31 - GenericsSetDictionary/02_GenericsComRestricoes/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_GenericsComRestricoes {
+    internal class Product : IComparable{
+        public string Name {  get; set; }
+        public double Value { get; set; }
+        
+        public Product(string name, double value) {
+            Name = name;
+            Value = value;
+        }
+
+        public override string ToString() {
+            return Name + " " + Value;
+        }
+
+        public int CompareTo(object obj) {
+            if(!(obj is Product)) {
+                throw new ArgumentException("Erro!");
+            }
+            Product other = obj as Product;
+            return Value.CompareTo(other.Value);
+        }
+    }
+}
